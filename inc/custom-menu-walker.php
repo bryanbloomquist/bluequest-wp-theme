edc693721000a290ed9187d259b65c4d45bf2e11
Bryan Bloomquist <bbloomquist@creedinteractive.com>
2021-10-05 09:19:43 -0500

    added target attribute to menu

--- inc/custom-menu-walker.php
+++ inc/custom-menu-walker.php
@@ -11,12 +11,13 @@ class Custom_Walker extends Walker_Nav_Menu {
         $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
         $class_names = implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
         $class_names = $class_names ? ' class="' . esc_attr( $class_names ). '"' : '';
+        $target = $item->target ? 'target="' . $item->target . '"' : null;
 
         $output .= '<li' . $class_names . '>';
         
         // if( $permalink && $permalink != '#' ) {
         if( $permalink ) {
-            $output .= '<a href="' . $permalink . '" class="nav-link">';
+            $output .= '<a href="' . $permalink . '" class="nav-link" ' . $target . '>';
         } else {
             $output .= '<span class="nav-item">';
         }
@@ -41,10 +42,12 @@ class Custom_Footer_Walker extends Walker_Nav_Menu {
     function start_el( &$output, $item, $depth=0, $args=array(), $id = 0) {
         $title = $item->title;
         $permalink = $item->url;
+        $target = $item->target ? 'target="' . $item->target . '"' : null;
+
         $output .= "<li class='nav-item'>";
         // if( $permalink && $permalink != '#' ) {
         if( $permalink ) {
-            $output .= '<a href="' . $permalink . '">' . $title . '</a>';
+            $output .= '<a href="' . $permalink . '" ' . $target . '>' . $title . '</a>';
         } else {
             $output .= '<span>' . $title . '</span>';
         }
